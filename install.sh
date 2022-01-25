8be71db307474b4fff5568a3f09dfa2faa941a86
Karan Sharma <hello@mrkaran.dev>
2022-01-25 21:16:04 +0530

    fix: dir paths in the install script

--- install.sh
+++ install.sh
@@ -13,29 +13,30 @@ check_dependencies() {
 	fi
 }
 
-setup_dirs() {
-    mkdir -p /etc/monschool-agent
-}
-
 download_binary() {
     mkdir -p /tmp/monschool-agent
     cd /tmp/monschool-agent ; curl -sL https://github.com/fossunited/monschool-agent/releases/download/v${VERSION}/monschool-agent_${VERSION}_linux_amd64.tar.gz | tar xz
 }
 
 binary_path() {
-    mv /tmp/monschool-agent/monschool-agent.bin /usr/local/bin
+    mv /tmp/monschool-agent/monschool-agent.bin /usr/bin
+}
+
+setup_config() {
+    mkdir -p /etc/monschool-agent
+    mv /tmp/monschool-agent/config.sample.toml /etc/monschool-agent/config.toml
 }
 
 setup_systemd() {
-    mv /tmp/monschool-agent/monschool-agent.service /etc/systemd/system/monschool-agent.service
+    mv /tmp/monschool-agent/deployment/monschool-agent.service /etc/systemd/system/monschool-agent.service
     systemctl daemon-reload
     systemctl enable --now monschool-agent
     systemctl status monschool-agent
 }
 
 
-
 check_dependencies
 download_binary
 binary_path
+setup_config
 setup_systemd
